3b3c391fd76ae9d8399d0dc6693f0ff2683a2868
njbmaliangkay30 <njbmaliangkay30@gmail.com>
2026-05-11 14:30:07 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useAppStore } from './store/useAppStore';
 import { Header } from './components/layout/Header';
 import { TabBar } from './components/layout/TabBar';
